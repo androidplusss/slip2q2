a3f9e47e53d95f5b04755781c218c75aca098377
androidplusss <madaneamit63@gmail.com>
2025-03-25 18:56:32 +0530

    AnotherActivity.java

---
+++ AnotherActivity.java
@@ -0,0 +1,10 @@
+package com.example.slip2q2audio;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class AnotherActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_another);
+    }
+}
